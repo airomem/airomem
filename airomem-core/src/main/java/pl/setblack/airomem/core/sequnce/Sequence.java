1820904e21d8b378b3cefe059e7732a1abfe1255
jarekratajski <jratajski@gmail.com>
2014-08-02 10:14:03 +0200

    atomicLong used for sequence

--- airomem-core/src/main/java/pl/setblack/airomem/core/sequnce/Sequence.java
+++ airomem-core/src/main/java/pl/setblack/airomem/core/sequnce/Sequence.java
@@ -1,9 +1,10 @@
 /*
- *  Copyright (c) Jarek Ratajski, Licensed under the Apache License, Version 2.0   http://www.apache.org/licenses/LICENSE-2.0 
+ *  Copyright (c) Jarek Ratajski, Licensed under the Apache License, Version 2.0   http://www.apache.org/licenses/LICENSE-2.0
  */
 package pl.setblack.airomem.core.sequnce;
 
 import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Helper object used to generate sequences of unique ids.
@@ -14,9 +15,9 @@ public class Sequence implements Serializable {
 
     private static final long serialVersionUID = 1l;
 
-    private long number;
+    private final AtomicLong number = new AtomicLong(0);
 
     public synchronized long generateId() {
-        return this.number++;
+        return this.number.incrementAndGet();
     }
 }
